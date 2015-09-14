2bee0c7b241b294b5b713de662bfe58fd6d5dcee
Arnaud Bonnevigne <arnaud.bonnevigne@wengo.com>
2015-09-14 15:40:26 +0200

    test

--- Bonvga/Bundle/Geoip2Bundle/Geoip2Bundle.php
+++ Bonvga/Bundle/Geoip2Bundle/Geoip2Bundle.php
@@ -3,7 +3,6 @@
 namespace Bonvga\Bundle\Geoip2Bundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class Geoip2Bundle extends Bundle
 {
