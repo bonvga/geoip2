82b108d71f78b6a13ff707aa79ba6d103197d30d
Arnaud Bonnevigne <arnaud.bonnevigne@wengo.com>
2015-09-14 12:38:05 +0200

    test

--- Bonvga/Bundle/Geoip2Bundle/Composer/ScriptHandler.php
+++ Bonvga/Bundle/Geoip2Bundle/Composer/ScriptHandler.php
@@ -1,15 +1,18 @@
 <?php
 namespace Bonvga\Bundle\GeoipBundle\Composer;
 
+use Composer\Script\CommandEvent;
+use Symfony\Component\Process\Process;
+
 class ScriptHandler
 {
 
-    static public function installDatabase()
+    static public function installDatabase(CommandEvent $event)
     {
+
+        var_dump($event);
 
         die('TOTO');
 
     }
-
-
 }
